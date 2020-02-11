dc4494357023d64276bd5cf19d04a410d367c8b1
inkfarer <inkfarer@gmail.com>
2020-02-11 23:23:51 +0200

    upcoming stages done

--- graphics/break.js
+++ graphics/break.js
@@ -1,3 +1,28 @@
+const mapNameToImagePath = {"Ancho-V Games": "stages/S2_Stage_Ancho-V_Games.png",
+"Arowana Mall":"stages/S2_Stage_Arowana_Mall.png",
+"Blackbelly Skatepark":"stages/S2_Stage_Blackbelly_Skatepark.png",
+"Camp Triggerfish":"stages/S2_Stage_Camp_Triggerfish.png",
+"Goby Arena":"stages/S2_Stage_Goby_Arena.png",
+"Humpback Pump Track":"stages/S2_Stage_Humpback_Pump_Track.png",
+"Inkblot Art Academy":"stages/S2_Stage_Inkblot_Art_Academy.png",
+"Kelp Dome":"stages/S2_Stage_Kelp_Dome.png",
+"MakoMart":"stages/S2_Stage_MakoMart.png",
+"Manta Maria":"stages/S2_Stage_Manta_Maria.png",
+"Moray Towers":"stages/S2_Stage_Moray_Towers.png",
+"Musselforge Fitness":"stages/S2_Stage_Musselforge_Fitness.png",
+"New Albacore Hotel":"stages/S2_Stage_New_Albacore_Hotel.png",
+"Piranha Pit":"stages/S2_Stage_Piranha_Pit.png",
+"Port Mackerel":"stages/S2_Stage_Port_Mackerel.png",
+"Shellendorf Institute":"stages/S2_Stage_Shellendorf_Institute.png",
+"Shifty Station":"stages/S2_Stage_Shifty_Station.png",
+"Snapper Canal":"stages/S2_Stage_Snapper_Canal.png",
+"Starfish Mainstage":"stages/S2_Stage_Starfish_Mainstage.png",
+"Sturgeon Shipyard":"stages/S2_Stage_Sturgeon_Shipyard.png",
+"The Reef":"stages/S2_Stage_The_Reef.png",
+"Wahoo World":"stages/S2_Stage_Wahoo_World.png",
+"Walleye Warehouse":"stages/S2_Stage_Walleye_Warehouse.png",
+"Skipper Pavilion":"stages/S2_Stage_Skipper_Pavilion.png"};
+
 const emptyTeamInfo = {
     name: "",
     logoUrl: "",
@@ -21,15 +46,39 @@ const musicShown = nodecg.Replicant('musicShown', {defaultValue: true});
 const currentBreakScene = nodecg.Replicant('currenBreakScene', { defaultValue: 'mainScene' });
 const nextTeamAInfo = nodecg.Replicant('nextTeamAInfo', {defaultValue: emptyTeamInfo});
 const nextTeamBInfo = nodecg.Replicant('nextTeamBINfo', {defaultValue: emptyTeamInfo});
+const currentMaplist = nodecg.Replicant('currentMaplist', {
+    defaultValue: [
+        { id: 0, name: 'Default map list' },
+        { map: 'Ancho-V Games', mode: 'Clam Blitz' },
+        { map: 'Ancho-V Games', mode: 'Tower Control' },
+        { map: 'Wahoo World', mode: 'Rainmaker' }
+    ]
+});
+const mapWinners = nodecg.Replicant('mapWinners', { defaultValue: [0, 0, 0, 0, 0, 0, 0] });
+const teamAInfo = nodecg.Replicant('teamAInfo', { defaultValue: emptyTeamInfo });
+const teamBInfo = nodecg.Replicant('teamBInfo', { defaultValue: emptyTeamInfo });
 
 //replicant changes
+currentMaplist.on('change', newValue => {
+	gsap.to('#upcomingStagesGrid', {duration: 0.5, opacity: 0, onComplete: function() {
+		clearUpcomingStages();
+		setMapCount(newValue.length - 1);
+		for (let i = 1; i < newValue.length; i++) {
+			addUpcomingStage(newValue[i], newValue.length - 1, i - 1);
+		}
+	}});
+	gsap.to('#upcomingStagesGrid', {duration: 0.5, opacity: 1, delay: 0.5});
+});
+
 bigTextValue.on('change', newValue => {
 	changeBreakMainText('breakFlavorText', newValue, "breakFlavorTextBG");
 	topBarText.text = newValue;
 });
+
 casterNames.on('change', newValue => {
 	changeBreakMainText('breakCasterNames', newValue, 'breakCasterNamesBG');
 });
+
 nowPlaying.on('change', newValue => {
 	if (!mSongEnabled.value) {
 		if (newValue.artist == undefined && newValue.song == undefined) {
@@ -39,7 +88,8 @@ nowPlaying.on('change', newValue => {
 			changeBreakMainText('breakSongText', songName, 'breakSongTextBG');
 		}		
 	}
-})
+});
+
 mSongEnabled.on('change', newValue => {
 	var value;
 	if (newValue) { value = nowPlayingManual.value; }
@@ -52,6 +102,7 @@ mSongEnabled.on('change', newValue => {
 		changeBreakMainText('breakSongText', songName, 'breakSongTextBG');
 	}		
 });
+
 nowPlayingManual.on('change', newValue => {
 	if (mSongEnabled.value) {
 		if (newValue.artist == undefined && newValue.song == undefined) {
@@ -62,6 +113,7 @@ nowPlayingManual.on('change', newValue => {
 		}		
 	}
 });
+
 musicShown.on('change', newValue => {
 	const musicElem = document.getElementById('musicWrapper')
 	if (newValue) {
@@ -70,32 +122,61 @@ musicShown.on('change', newValue => {
 		gsap.to('#musicWrapper', 0.5, {opacity: 0});
 	}
 });
+
 currentBreakScene.on('change', newValue => {
 	if (newValue === "mainScene") {
 		hideNextUp();
 		hideTopBar();
+		hideMaps();
 		showMainScene(0);
 	} else if (newValue === "nextUp") {
 		showTopBar(1.25);
 		hideMainScene();
+		hideMaps();
 		showNextUp();
 	} else if (newValue === "maps") {
 		showTopBar(1.25);
 		hideMainScene();
+		hideNextUp();
+		showMaps();
 	}
 });
 
 nextTeamAInfo.on('change', newValue => {
-	console.log(newValue);
 	teamAImage.src = newValue.logoUrl;
 	teamAName.text = newValue.name;
 	addTeamPlayers('A', newValue.players);
 });
 nextTeamBInfo.on('change', newValue => {
-	console.log(newValue);
 	teamBImage.src = newValue.logoUrl;
 	teamBName.text = newValue.name;
 	addTeamPlayers('B', newValue.players);
+});
+
+mapWinners.on('change', (newValue, oldValue) => {
+	if (!oldValue) {
+		//assume we're on first load
+		for (let i = 0; i < newValue.length; i++) {
+			const element = newValue[i];
+			if (element != 0) {
+				addWinner(i, element);
+			}
+		}
+	} else {
+		for (let i = 0; i < newValue.length; i++) {
+			const element = newValue[i];
+			const oldElement = oldValue[i];
+			if (element != oldElement) {
+				if (element == 0) {
+					removeWinner(i);
+				} else if (oldElement == 0) {
+					addWinner(i, element);
+				} else {
+					updateWinner(i, element);
+				}
+			}
+		}
+	}
 });
 
 //looping background
@@ -170,7 +251,7 @@ function addTopBarAnim(i) {
 		}
 	}}))
 	.add(gsap.to('#topBarInfoText, #topBarInfoIcon', 0.5, {opacity: 1, delay: 0.5}))
-	.add(gsap.to({}, 5, {}));
+	.add(gsap.to({}, 10, {}));
 	if (i == 1) {
 		topBarInfoTL.to({}, 0.01, {delay: -0.01, onComplete: function() {startTopBarTextLoop()}});
 	}
@@ -233,13 +314,132 @@ function addTeamPlayers(teamNo, players) {
 		if (teamNo === 'A') {
 			playerText.align = "right";
 			playerText.classList.add('teamPlayerA');
-			teamAInfo.appendChild(playerText);
+			document.getElementById('teamAInfo').appendChild(playerText);
 		} else if (teamNo === 'B') {
 			playerText.align = "left";
 			playerText.classList.add('teamPlayerB')
-			teamBInfo.appendChild(playerText);
+			document.getElementById('teamBInfo').appendChild(playerText);
 		}
 	}
+}
+
+function setMapCount(count) {
+	/*grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;*/
+	var styleVal = '';
+	for (let i = 0; i < count; i++) {
+		styleVal += '1fr ';
+	}
+	upcomingStagesGrid.style.gridTemplateColumns = styleVal;
+	upcomingStagesGrid.style.width = '1700px';
+	if (count == 3) {
+		upcomingStagesGrid.style.width = '1400px';
+	}
+}
+
+function addUpcomingStage(stageInfo, count, id) {
+	/*<div class="stageBox">
+			<img src="stages/S2_Stage_MakoMart.png" class="mapsStagePic">
+			<div class="mapsModeName">Clam Blitz</div>
+			<div class="mapsStageName">MakoMart</div>
+			<div class="mapsNameGradient"></div>
+			<div class="mapsWinnerName">Crustacean Crusaders</div>
+			<div class="winnerGradient"></div>
+		</div>*/
+
+	const stageBox = document.createElement('div');
+	stageBox.classList.add('stageBox');
+	stageBox.id = 'stageBox_' + id;
+
+	const stageName = document.createElement('div');
+	stageName.innerText = stageInfo.map;
+	stageName.classList.add('mapsStageName');
+	stageBox.appendChild(stageName);
+
+	const modeName = document.createElement('div');
+	modeName.innerText = stageInfo.mode;
+	modeName.classList.add('mapsModeName');
+	stageBox.appendChild(modeName);
+
+	const mapGradient = document.createElement('div');
+	mapGradient.classList.add('mapsNameGradient');
+	stageBox.appendChild(mapGradient);
+
+	const stageImage = document.createElement('img');
+	if (count == 3) {
+		stageImage.classList.add('mapsStagePic3Maps');
+	}
+	stageImage.classList.add('mapsStagePic');
+	stageImage.src = mapNameToImagePath[stageInfo.map];
+	stageBox.appendChild(stageImage);
+
+	document.getElementById('upcomingStagesGrid').appendChild(stageBox);
+	if (mapWinners.value[id] != 0) {
+		addWinner(id, mapWinners.value[id]);
+	}
+}
+
+function addWinner(index, value) {
+	const mapElem = document.querySelector('div#stageBox_' + index);
+	if (!mapElem) {
+		//we're in trouble
+		return;
+	}
+
+	const name = mapElem.querySelector('.mapsWinnerName');
+	const gradient = mapElem.querySelector('.winnerGradient');
+
+	if (name == null && gradient == null) {
+		const winnerName = document.createElement('div');
+		winnerName.classList.add('mapsWinnerName');
+		if (value == 1)  {
+			winnerName.innerText = teamAInfo.value.name;
+		} else if (value == 2) {
+			winnerName.innerText = teamBInfo.value.name;
+		}
+		mapElem.appendChild(winnerName);
+
+		const winnerGradient = document.createElement('div');
+		winnerGradient.classList.add('winnerGradient');
+		mapElem.appendChild(winnerGradient);
+
+		gsap.fromTo(winnerName, {opacity: 0}, {duration: 0.5, opacity: 1});
+		gsap.fromTo(winnerGradient, {opacity: 0}, {duration: 0.5, opacity: 1});
+	}
+}
+
+function updateWinner(index, newValue) {
+	const mapElem = document.querySelector('div#stageBox_' + index);
+	if (!mapElem) {
+		return;
+	}
+
+	const name = mapElem.querySelector('.mapsWinnerName');
+	
+	if (newValue == 1) {
+		name.innerText = teamAInfo.value.name;
+	} else if (newValue == 2) {
+		name.innerText = teamBInfo.value.name;
+	}
+}
+
+function removeWinner(index) {
+	const mapElem = document.querySelector('div#stageBox_' + index);
+	if (!mapElem) {
+		return;
+	}
+
+	const name = mapElem.querySelector('.mapsWinnerName');
+	const gradient = mapElem.querySelector('.winnerGradient');
+	
+	gsap.to(name, {duration: 0.5, opacity: 0});
+	gsap.to(gradient, {duration: 0.5, opacity: 0, onComplete: function() {
+		name.parentElement.removeChild(name);
+		gradient.parentElement.removeChild(gradient);
+	}});
+}
+
+function clearUpcomingStages() {
+	document.getElementById('upcomingStagesGrid').innerHTML = '';
 }
 
 
@@ -284,4 +484,12 @@ function showNextUp(delay) {
 
 function hideNextUp() {
 	gsap.to("#nextUp", 1.5, {ease: 'power3.inOut',  top: 1080});
+}
+
+function showMaps() {
+	gsap.fromTo("#nextMaps", {top: -1080}, {top: 0, duration: 1.5, ease: 'power3.inOut'});
+}
+
+function hideMaps() {
+	gsap.to("#nextMaps", 1.5, {ease: 'power3.inOut',  top: 1080});
 }
